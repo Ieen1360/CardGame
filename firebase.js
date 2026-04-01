4cd0ec1d4d3a71cd4e3c530c60ed1bf55f0e4c11
Ieen <dacruzdesouzadavi@gmail.com>
2026-04-01 16:24:42 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,19 @@
+// Configurações do seu projeto
 const firebaseConfig = {
-  apiKey: "AIzaSyAMdBDtEDuV23aJelAMlslqEoikZ6VXMIU",
-  authDomain: "cardgame-e2501.firebaseapp.com",
-  databaseURL: "https://cardgame-e2501-default-rtdb.firebaseio.com",
-  projectId: "cardgame-e2501",
-  storageBucket: "cardgame-e2501.appspot.com",
-  messagingSenderId: "45522667511",
-  appId: "1:45522667511:web:55242fdc999ff84fa290f2"
+    apiKey: "AIzaSyAMdBDtEDuV23aJelAMlslqEoikZ6VXMIU",
+    authDomain: "cardgame-e2501.firebaseapp.com",
+    databaseURL: "https://cardgame-e2501-default-rtdb.firebaseio.com",
+    projectId: "cardgame-e2501",
+    storageBucket: "cardgame-e2501.appspot.com",
+    messagingSenderId: "45522667511",
+    appId: "1:45522667511:web:55242fdc999ff84fa290f2"
 };
 
-// Inicialização segura
+// Inicializa o Firebase apenas se não houver um app rodando
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
-} else {
-    firebase.app(); // Se já estiver inicializado, usa o existente
 }
 
-window.db = firebase.database();
+// Exporta para o escopo global (isso resolve o "auth is not defined")
 window.auth = firebase.auth();
+window.database = firebase.database();
