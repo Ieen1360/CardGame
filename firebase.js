988608c5e55eed12d13ada53ebeae9b9c091d02f
Ieen <dacruzdesouzadavi@gmail.com>
2026-04-01 15:57:03 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,19 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAMdBDtEDuV23aJelAMlslqEoikZ6VXMIU",
-    authDomain: "cardgame-e2501.firebaseapp.com",
-    databaseURL: "https://cardgame-e2501-default-rtdb.firebaseio.com",
-    projectId: "cardgame-e2501",
-    storageBucket: "cardgame-e2501.appspot.com",
-    messagingSenderId: "45522667511",
-    appId: "1:45522667511:web:55242fdc999ff84fa290f2"
+  apiKey: "AIzaSyAMdBDtEDuV23aJelAMlslqEoikZ6VXMIU",
+  authDomain: "cardgame-e2501.firebaseapp.com",
+  databaseURL: "https://cardgame-e2501-default-rtdb.firebaseio.com",
+  projectId: "cardgame-e2501",
+  storageBucket: "cardgame-e2501.appspot.com",
+  messagingSenderId: "45522667511",
+  appId: "1:45522667511:web:55242fdc999ff84fa290f2"
 };
 
-// Inicializa o App
+// Inicialização segura
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
+} else {
+    firebase.app(); // Se já estiver inicializado, usa o existente
 }
 
-// Exporta para o objeto window para o script.js enxergar
 window.db = firebase.database();
 window.auth = firebase.auth();
