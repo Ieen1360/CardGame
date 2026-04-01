fadc55d7bb6e23ea766cbeebe4784d22ac48b6e1
Ieen <dacruzdesouzadavi@gmail.com>
2026-04-01 15:44:10 -0300

    Refactor Firebase initialization and export

--- firebase.js
+++ firebase.js
@@ -1,18 +1,18 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAMdBDtEDuV23aJelAMlslqEoikZ6VXMIU",
-  authDomain: "cardgame-e2501.firebaseapp.com",
-  databaseURL: "https://cardgame-e2501-default-rtdb.firebaseio.com",
-  projectId: "cardgame-e2501",
-  storageBucket: "cardgame-e2501.appspot.com",
-  messagingSenderId: "45522667511",
-  appId: "1:45522667511:web:55242fdc999ff84fa290f2"
+    apiKey: "AIzaSyAMdBDtEDuV23aJelAMlslqEoikZ6VXMIU",
+    authDomain: "cardgame-e2501.firebaseapp.com",
+    databaseURL: "https://cardgame-e2501-default-rtdb.firebaseio.com",
+    projectId: "cardgame-e2501",
+    storageBucket: "cardgame-e2501.appspot.com",
+    messagingSenderId: "45522667511",
+    appId: "1:45522667511:web:55242fdc999ff84fa290f2"
 };
 
-// Inicializa o Firebase
+// Inicializa o App
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
-// Torna os serviços globais para o script.js
+// Exporta para o objeto window para o script.js enxergar
 window.db = firebase.database();
-window.auth = firebase.auth(); // <-- ESSA LINHA É VITAL
+window.auth = firebase.auth();
